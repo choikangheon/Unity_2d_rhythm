d63ea53300484ac289c548e66dc5dc903d7d694c
jaeho <aktmzm5321@naver.com>
2021-12-28 19:11:07 +0900

    라인 생성

---
+++ Rhythm Game/Assets/Scripts/GameManager.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{   
+    //GameManager¸¦ ½Ì±Û Åæ Ã³¸®ÇÕ´Ï´Ù.
+    public static GameManager instance { get; set; }
+    private void Awake()
+    {
+        if (instance == null) instance = this;
+        else if (instance != this) Destroy(gameObject);
+
+    }
+    public float noteSpeed; 
+      
+         
+  
+    void Start()
+    {
+        
+    }
+
+   
+    void Update()
+    {
+        
+    }
+}
