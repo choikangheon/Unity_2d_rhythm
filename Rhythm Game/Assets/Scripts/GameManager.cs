8e26f904ad671d29f0b99d829dc45882b6a7b0f2
choikangheon <choikangheon@naver.com>
2022-01-02 17:00:45 +0900

    충돌및 충돌에 따른 저지먼트

--- Rhythm Game/Assets/Scripts/GameManager.cs
+++ Rhythm Game/Assets/Scripts/GameManager.cs
@@ -5,16 +5,17 @@ using UnityEngine;
 public class GameManager : MonoBehaviour
 {   
     //GameManager¸¦ ½Ì±Û Åæ Ã³¸®ÇÕ´Ï´Ù.
-    public static GameManager instance { get; set; }
+    public static GameManager instance { get; set; } // GameManger °´Ã¼
     private void Awake()
     {
         if (instance == null) instance = this;
         else if (instance != this) Destroy(gameObject);
 
     }
-    public float noteSpeed; 
+    public float noteSpeed;  // GameMangerÀÇ noteSpeed °ª 
       
          
+ public enum judges { NONE = 0 , BAD, GOOD, PERFECT , MISS};
   
     void Start()
     {
